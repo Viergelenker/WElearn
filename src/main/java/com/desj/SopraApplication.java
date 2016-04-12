1d916ea4ecef35224de97cae5469695a5799e8e4
Julien Vollweiter <viergelenker@web.de>
2016-04-12 21:33:03 +0200

    Inital setup of the sopra project

---
+++ src/main/java/com/desj/SopraApplication.java
@@ -0,0 +1,12 @@
+package com.desj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopraApplication.class, args);
+	}
+}
