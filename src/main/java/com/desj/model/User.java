333beb14754924db0df724244b0cb4fb0bea1a61
Julien Vollweiter <viergelenker@web.de>
2016-04-14 12:34:30 +0200

    Added a new user class

---
+++ src/main/java/com/desj/model/User.java
@@ -0,0 +1,27 @@
+package com.desj.model;
+
+/**
+ * Created by Julien on 14.04.16.
+ */
+
+public class User {
+
+    private String uname;
+    private String password;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
