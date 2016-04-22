b2e030de9372c80b2e0e58003ae749baa786b4d4
Julien Vollweiter <viergelenker@web.de>
2016-04-22 20:10:37 +0200

    Bootstrap via link included, livereload is also working now

--- src/main/java/com/desj/controller/LoginController.java
+++ src/main/java/com/desj/controller/LoginController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by Julien on 13.04.16.
  */
 @Controller
-public class HelloWorldController {
+public class LoginController {
 
     @RequestMapping("/")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "helloWorld";
+        return "login";
     }
 }
