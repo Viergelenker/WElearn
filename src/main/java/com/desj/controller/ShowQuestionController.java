7925cbe3edfb8a4f16a80f45ca5c154fa03257a4
mishanka <desi_mi@abv.bg>
2016-07-10 21:51:37 +0200

    commit didnt commit

--- src/main/java/com/desj/controller/ShowQuestionController.java
+++ src/main/java/com/desj/controller/ShowQuestionController.java
@@ -1,9 +1,7 @@
 package com.desj.controller;
 
-import com.desj.model.LearningGroupRepository;
-import com.desj.model.Question;
-import com.desj.model.User;
-import com.desj.model.UserRepository;
+import com.desj.model.*;
+import com.desj.service.QuestionCommentService;
 import com.desj.service.QuestionService;
 import com.desj.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +28,9 @@ public class ShowQuestionController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private QuestionCommentService questionCommentService;
+
     @RequestMapping(value = "/showQuestion")
     public String showQuestion(@RequestParam("learningGroupId") Integer learningGroupId, Model model) {
 
@@ -41,6 +42,9 @@ public class ShowQuestionController {
             model.addAttribute("question", question);
             model.addAttribute("error", false);
             model.addAttribute("creator", userRepository.getOne(question.getCreator().getId()));
+            model.addAttribute("questionComment", new QuestionComment());
+            model.addAttribute("allQuestionComments",questionCommentService.getAllCommentsOfQuestion(question));
+
         }
         else {
             model.addAttribute("error", true);
